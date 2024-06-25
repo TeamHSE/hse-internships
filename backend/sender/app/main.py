a590eb50dbc8e525c6168e58d22e1949f50ecbd3
aleks <aleks@noreply.com>
2024-06-25 10:41:04 +0500

    refactor(parser): move CORS URLs to compose

--- backend/sender/app/main.py
+++ backend/sender/app/main.py
@@ -9,9 +9,9 @@ from email.mime.multipart import MIMEMultipart
 
 TOKEN = os.environ.get("BOT_TOKEN")
 EMAIL_ADRESS = os.environ.get("EMAIL")
-PASSWORD=os.environ.get("PASSWORD")
-PASSWORD=os.environ.get("PASSWORD")
-CORS_URLS=os.environ.get("CORS_URLS")
+PASSWORD = os.environ.get("PASSWORD")
+PASSWORD = os.environ.get("PASSWORD")
+CORS_URLS = os.environ.get("CORS_URLS")
 
 bot = telebot.TeleBot(TOKEN)
 server = smtp.SMTP_SSL('smtp.yandex.com.tr:465')
@@ -19,12 +19,14 @@ server.ehlo(EMAIL_ADRESS)
 server.login(EMAIL_ADRESS, PASSWORD)
 
 app = Flask(__name__)
-CORS(app, origins=[CORS_URLS.split(',')], supports_credentials=True, methods=['GET', 'POST'])
+CORS(app, origins=CORS_URLS.split(','), supports_credentials=True, methods=['GET', 'POST'])
+
 
 @bot.message_handler()
 def send_chat_id(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, f"Your chat ID is: ```{chat_id}```", parse_mode='MarkdownV2')
+
 
 def send_email(target_email, event_name, text):
     msg = MIMEMultipart('alternative')
@@ -36,16 +38,17 @@ def send_email(target_email, event_name, text):
     server.auth_plain()
     server.sendmail(EMAIL_ADRESS, target_email, msg.as_string())
 
+
 # Endpoint to send messages via GET request
 @app.route('/send_message', methods=['POST'])
 def send_message():
     users = request.json.get('users')
-    event_name  = request.json.get('event_name')
+    event_name = request.json.get('event_name')
     event_link = request.json.get('event_link')
-    
+
     if not event_name or not event_link or not users:
         return 'Missing params', 400
-    
+
     msg = f"""Привет, появилось новое событие:
 Событие: {event_name}
 Ссылка: {event_link}
@@ -68,10 +71,12 @@ def send_message():
                     answer, code = f'An error occurred: {str(e)}', 500
     return answer, code
 
+
 # Function to run Flask and bot in separate threads
 def start_flask_and_bot():
     threading.Thread(target=lambda: app.run(host="0.0.0.0", debug=True, use_reloader=False)).start()
     bot.polling()
+
 
 if __name__ == '__main__':
     start_flask_and_bot()
